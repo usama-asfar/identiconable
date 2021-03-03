6628792cb0aac09d97373473bbd3959f211af16a
usama <usamaasfar.mua@gmail.com>
2021-03-03 11:58:40 +0530

    update mix

--- mix.exs
+++ mix.exs
@@ -7,8 +7,16 @@ defmodule Identiconable.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      licenses: MIT,
+      name: "identiconable",
+      source_url: "https://github.com/usama-asfar/identiconable.git"
     ]
+  end
+
+  defp description() do
+    "An Identicon is a visual representation of a hash value, usually of an IP address, that serves to identify a user of a computer system as a form of avatar while protecting the users' privacy - Wikipedia."
   end
 
   # Run "mix help compile.app" to learn about applications.
