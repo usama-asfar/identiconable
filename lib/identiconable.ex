86106747c8eb50f2cd64d8d13802ace25f3361df
usama <usamaasfar.mua@gmail.com>
2021-03-03 11:45:30 +0530

    initialize commit

---
+++ lib/identiconable.ex
@@ -0,0 +1,18 @@
+defmodule Identiconable do
+  @moduledoc """
+  Documentation for `Identiconable`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Identiconable.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/identiconable_test.exs
@@ -0,0 +1,8 @@
+defmodule IdenticonableTest do
+  use ExUnit.Case
+  doctest Identiconable
+
+  test "greets the world" do
+    assert Identiconable.hello() == :world
+  end
+end
